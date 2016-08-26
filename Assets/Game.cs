79665d78321c1f4b3120f746ae412927f6998071
Esa Niemi <esa.niemi@wapice.com>
2016-08-26 23:03:30 +0300

    Working klok

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -1,10 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections;
 
 public class Game : MonoBehaviour {
     public Canvas canvas;
-    public GameObject debugCanvas;
+    public Text text;
+    public GameObject debugCanvas, clockArm;
+    enum State {STARTING, STARTED, ENDED};
+    State state = State.STARTING;
+    public bool isStarted = false;
+    float duration;
+    float deadline;
 
     void Start(){
         if (Debug.isDebugBuild)
@@ -12,11 +19,27 @@ public class Game : MonoBehaviour {
             debugCanvas.SetActive(true);
         }
     }
+
+    void FixedUpdate()
+    {
+        if (state == State.STARTED)
+        {
+            if (Time.time >= deadline)
+                state = State.ENDED;
+            else
+                clockArm.transform.Rotate(Vector3.up * (Time.deltaTime / duration * 360), Space.Self);
+        }
+    }
+
     /// <summary>
     /// Starts the game counters and enables player controls
     /// </summary>
     public void startGame()
     {
+        duration = float.Parse(text.text)*60;
+        deadline = Time.time + duration;
         canvas.enabled = false;
+        debugCanvas.SetActive(false);
+        state = State.STARTED;
     }
 }
