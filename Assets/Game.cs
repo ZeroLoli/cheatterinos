827f9a5c40b93f20755091f8d467209c3be84cba
Esa Niemi <esa.niemi@wapice.com>
2016-08-26 22:26:58 +0300

    Timer slider and new graphics

---
+++ Assets/Game.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+    public Canvas canvas;
+    public GameObject debugCanvas;
+
+    void Start(){
+        if (Debug.isDebugBuild)
+        {
+            debugCanvas.SetActive(true);
+        }
+    }
+    /// <summary>
+    /// Starts the game counters and enables player controls
+    /// </summary>
+    public void startGame()
+    {
+        canvas.enabled = false;
+    }
+}
---
+++ Assets/SliderLabelValue.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class SliderLabelValue : MonoBehaviour
+{
+    public Slider slider;
+    public Text text;
+
+    void Start()
+    {
+        slider.onValueChanged.AddListener(ChangeValue);
+        text.text = (Math.Round(slider.value * 2, MidpointRounding.AwayFromZero) / 2).ToString();
+    }
+
+    void ChangeValue(float value)
+    {
+        text.text = (Math.Round(value*2, MidpointRounding.AwayFromZero)/2).ToString();
+    }
+}
