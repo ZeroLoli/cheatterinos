b1675e49e5a00eff7cdfaac950803ce916e89479
Valtteri Rasila <xzero-94@hotmail.com>
2016-08-28 16:57:28 +0300

    drawing had one more bug; fixed

--- Assets/drawing.cs
+++ Assets/drawing.cs
@@ -11,42 +11,53 @@ public class Drawing : MonoBehaviour {
 
     void Update() {
         Debug.Log(Physics.Raycast(pen.transform.position, Vector3.down));
-        // Start drawing new line; need something
-        if (/*(Input.GetKeyDown(KeyCode.Mouse0) ||*/ (!lineDraw && penDown) && Physics.Raycast(pen.transform.position, Vector3.down)) {
-            GameObject myLine = new GameObject();
-            myLine.transform.position = pen.transform.position;
-            myLine.AddComponent<LineRenderer>();
-            line = myLine.GetComponent<LineRenderer>();
-            line.material = mat;
-            line.receiveShadows = true;
-            //line.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
-            line.SetVertexCount(1);
-            line.SetColors(Color.black, Color.black);
-            line.SetWidth(0.002f, 0.002f);
-            line.useWorldSpace = true;
-            nodeList = new List<Vector3>();
-            line.SetPosition(0, transform.localPosition);
-            //penDown = true;
-            lineDraw = true;
-        }
-        // Continue drawing as long as something to draw on
-        if (lineDraw && penDown && Physics.Raycast(pen.transform.position, Vector3.down)) {
-            nodeList.Add(pen.transform.position);
-            line.SetVertexCount(nodeList.Count);
-            line.SetPosition(nodeList.Count - 1, (Vector3)nodeList[nodeList.Count - 1]);
-        }
-        // Nothing to draw on
-        if (!Physics.Raycast(pen.transform.position, Vector3.down)) {
+        //Raise pen
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            penDown = false;
             lineDraw = false;
         }
+        // Nothing to draw on
+        if (!Physics.Raycast(pen.transform.position, Vector3.down))
+        {
+            lineDraw = false;
+        }
+        // Start drawing new line; need something
+        else if (/*(Input.GetKeyDown(KeyCode.Mouse0) ||*/ penDown && Physics.Raycast(pen.transform.position, Vector3.down))
+        {
+            if (!lineDraw) {
+                GameObject myLine = new GameObject();
+                myLine.transform.position = pen.transform.position;
+                myLine.AddComponent<LineRenderer>();
+                line = myLine.GetComponent<LineRenderer>();
+                line.material = mat;
+                line.receiveShadows = true;
+                //line.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                line.SetVertexCount(1);
+                line.SetColors(Color.black, Color.black);
+                line.SetWidth(0.002f, 0.002f);
+                line.useWorldSpace = true;
+                nodeList = new List<Vector3>();
+                line.SetPosition(0, transform.localPosition);
+                //penDown = true;
+                lineDraw = true;
+            }
+            else {
+                nodeList.Add(pen.transform.position);
+                line.SetVertexCount(nodeList.Count);
+                line.SetPosition(nodeList.Count - 1, (Vector3)nodeList[nodeList.Count - 1]);
+            }
+        }
+        // Continue drawing as long as something to draw on
+        //if (lineDraw && penDown && Physics.Raycast(pen.transform.position, Vector3.down)) {
+        //    nodeList.Add(pen.transform.position);
+        //    line.SetVertexCount(nodeList.Count);
+        //    line.SetPosition(nodeList.Count - 1, (Vector3)nodeList[nodeList.Count - 1]);
+        //}
         // Lower pen
-        if ((Input.GetKeyDown(KeyCode.Mouse0))) {
+        if ((Input.GetKeyDown(KeyCode.Mouse0)))
+        {
             penDown = true;
         }
-        // Raise pen
-        if (Input.GetKeyUp(KeyCode.Mouse0)) {
-            penDown = false;
-        }
-        //Physics.Raycast(pen.transform.position, Vector3.down, out hit)
     }
 }
